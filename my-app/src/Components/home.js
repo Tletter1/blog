74328bf2b2db92815427185e187d86e2741c77f1
OwennD1 <owenn.dart@gmail.com>
2025-07-30 15:50:43 +0200

    aa

---
+++ my-app/src/Components/home.js
@@ -0,0 +1,22 @@
+import './home.css';
+
+function Home() {
+  return (
+    <div>
+      <header className="orange-header">
+        Owenn Dartois - Blog
+      </header>
+
+      <main className="main-content">
+        <h1>Welcome to my Home component 👋</h1>
+        <p>This is loaded from a separate file.</p>
+      </main>
+
+      <footer className="orange-footer">
+        &copy; 2025 - Blog
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
